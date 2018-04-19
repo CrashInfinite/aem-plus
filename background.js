30a0530a3c8e35f7bd345fbee67c5e2f903321b7
Andrew Raynes <hello@raynes.io>
2018-04-19 20:56:43 +0100

    Bit of HTML

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'outerBounds': {
-      'width': 400,
-      'height': 500
-    }
-  });
+var customParam = encodeURI('customparam=1');
+chrome.browserAction.onClicked.addListener(function (tab) {
+  var url = tab.url;
+  var hashStart = (url.indexOf('#') === -1) ? url.length : url.indexOf('#');
+  var querySymbol = (url.indexOf('?') === -1) ? '?' : '&';
+  var newUrl = url.substring(0, hashStart) + querySymbol + customParam +
+               url.substring(hashStart);
+
+  chrome.tabs.update(tab.id, {url: newUrl});
 });
